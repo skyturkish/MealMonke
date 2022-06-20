4da0eef97934537e40a79872e483b55f7d0521f1
sky <gokturk.acr2002@gmail.com>
2022-06-20 20:07:09 +0300

    localization, splash screen, launcher icon comp

---
+++ lib/core/constants/app/app_constants.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+class ApplicationConstants {
+  static const APPLICATION_TITLE = 'shopping title';
+  static const TRANSLATIONS_ASSET_PATH = 'assets/translations';
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping/core/constants/app/app_constants.dart';
+import 'package:shopping/core/init/translations/language_manager.dart';
+import 'package:shopping/home.dart';
+
+void main() async {
+  await _init();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: LanguageManager.instance.supportedLocales,
+      path: ApplicationConstants.TRANSLATIONS_ASSET_PATH,
+      startLocale: LanguageManager.instance.trLocale,
+      child: const MyApp(),
+    ),
+  );
+}
+
+Future<void> _init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      title: ApplicationConstants.APPLICATION_TITLE,
+      home: const TempView(),
+    );
+  }
+}
